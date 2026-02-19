de9e90599697872001c8bba42c63fbe788c27ca0
hurshidbe <egamberdiyevx8@gmail.com>
2026-02-19 13:18:16 +0500

    new solvings added

--- leet.js
+++ leet.js
@@ -14,3 +14,55 @@
 //     return n.toString() === n.toString().split('').reverse().join('')
 // }
 // console.log(isPalindrome(111111))
+
+// // 3
+// function twoSum(n,t){
+//     for (let index=0; index<n.length; index++){
+//         for (let i=0;i<n.length;i++){
+//             if (index===i) i=i+1
+//             if (n[index]+n[i]===t)
+//                 return [index, i]
+//         }
+//     }
+// }
+// console.log(twoSum([1,2,3,4],3))
+
+// // 4
+// function plusOne(d){
+//     let r= []
+//     let res= parseInt(d.join(''))
+//     const result = (res+1).toString().split('')
+//     for (const val of result){
+//         r.push(parseInt(val))
+//     }
+//     return r
+// }
+// console.log(plusOne([1,2,3,4]))
+
+// // 5
+// function addTwoNumbers(x,y){
+//     const nx= parseInt(x.toString().split('').reverse().join(''))
+//     const ny= parseInt(y.toString().split('').reverse().join(''))
+//     const rs= (ny+nx).toString().split('').reverse()
+//     let result =[]
+//     for (const val of rs){
+//         result.push(parseInt(val))
+//     }
+//     return result
+// }
+// console.log(addTwoNumbers(11, 12))
+
+// // 6
+// var isPalindrome = function(x) {
+//     return x.toString().split('')===x.toString().split('').reverse()
+// };
+// console.log(isPalindrome(121))
+
+// // 7
+// function map(arr, fn) {
+//     let result =[]
+// for (let i=0;i<arr.length;i++){
+//     result.push(fn(arr[i], i))
+// }
+// return result
+// }
